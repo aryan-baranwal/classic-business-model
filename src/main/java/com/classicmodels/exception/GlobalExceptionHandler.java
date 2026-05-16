9fa6c566d491436a3b09ff993f3983dd37e86331
Aman Raj <aman.raj@alrium.com>
2026-05-16 14:45:43 +0530

    [Aman Raj] : added : Service layer with exception and global exception handelling

---
+++ src/main/java/com/classicmodels/exception/GlobalExceptionHandler.java
@@ -0,0 +1,68 @@
+package com.classicmodels.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(
+            EmployeeNotFoundException.class
+    )
+    public ResponseEntity<String>
+    handleEmployeeNotFoundException(
+            EmployeeNotFoundException exception
+    ) {
+
+        return new ResponseEntity<>(
+                exception.getMessage(),
+                HttpStatus.NOT_FOUND
+        );
+    }
+
+    @ExceptionHandler(
+            MethodArgumentNotValidException.class
+    )
+    public ResponseEntity<Map<String, String>>
+    handleValidationException(
+            MethodArgumentNotValidException exception
+    ) {
+
+        Map<String, String> errors =
+                new HashMap<>();
+
+        exception.getBindingResult()
+                .getFieldErrors()
+                .forEach(error ->
+                        errors.put(
+                                error.getField(),
+                                error.getDefaultMessage()
+                        )
+                );
+
+        return new ResponseEntity<>(
+                errors,
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String>
+    handleGenericException(
+            Exception exception
+    ) {
+
+        return new ResponseEntity<>(
+                "Internal Server Error : "
+                        + exception.getMessage(),
+                HttpStatus.INTERNAL_SERVER_ERROR
+        );
+    }
+}
