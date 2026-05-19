f9dd12ac400acada6ee58189b122c475c64a5511
shivani <shivaniavanikkarasi@gmail.com>
2026-05-19 20:06:14 +0530

    [Shivani S] : Modified : Security Config Code

--- src/main/java/com/classicmodels/config/SecurityConfig.java
+++ src/main/java/com/classicmodels/config/SecurityConfig.java
@@ -28,7 +28,8 @@ public class SecurityConfig {
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
                                 "/v3/api-docs/**",
-                                "/api/**"
+                                "/api/**",
+                                "/orders/**"
                         ).permitAll()
 
                         .anyRequest().authenticated()
